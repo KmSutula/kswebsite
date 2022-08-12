2cb7036954a4e1ef5ead8374efa95b40e50381ad
Kaleigh <94947661+KmSutula@users.noreply.github.com>
2022-08-12 13:38:55 -0500

    multi functional work samples

--- pages/mywork.js
+++ pages/mywork.js
@@ -2,6 +2,10 @@ const sampleImgs = document.querySelectorAll(".sample");
 const sampleDescr = document.querySelectorAll("div.description");
 const sampleCont = document.getElementById("worksamplecontainer");
 
+const workSamps = document.querySelectorAll(".worksample");
+const rightButton = document.getElementById("right-arrow");
+const leftButton = document.getElementById("left-arrow");
+
 // sampleImgs.forEach((img) => {
 //   img.addEventListener("mouseenter", () => {
 //     img.classList.add("hover");
@@ -15,3 +19,30 @@ const sampleCont = document.getElementById("worksamplecontainer");
 //       }
 //     });
 //   }
+let activeSample = 0;
+
+rightButton.addEventListener("click", () => {
+  activeSample++;
+  if (activeSample > workSamps.length - 1) {
+    activeSample = 0;
+  }
+  makeActive();
+
+  //more code goes here after functions created
+});
+
+leftButton.addEventListener("click", () => {
+  activeSample--;
+  if (activeSample < 0) {
+    activeSample = workSamps.length - 1;
+  }
+  makeActive();
+});
+
+function makeActive() {
+  workSamps.forEach((sample) => {
+    sample.classList.remove("active");
+  });
+
+  workSamps[activeSample].classList.add("active");
+}
