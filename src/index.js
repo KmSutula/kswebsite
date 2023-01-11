525673ab17ca0e9b6e27cb285352f9068e0b2c33
Kaleigh <94947661+KmSutula@users.noreply.github.com>
2023-01-11 11:55:51 -0600

    updates to my work, added style components

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
+import sideNavData from "./sideNavData.js";
+
 const quoteOne = document.querySelectorAll(".quote-one");
 const quoteTwo = document.querySelectorAll(".quote-two");
 const loadBox = document.getElementById("load-box");
 const feDev = document.getElementById("fe-dev");
 const opera = document.getElementById("opera");
 const dogMom = document.getElementById("dog-mom");
-const footer = document.getElementById("footer");
+const interactiveNav = document.getElementById("interactive-nav");
+const seeMore = document.getElementById("click-to-see");
+const myWork = document.getElementById("my-work-section");
+const experience = document.getElementById("experience-section");
+const contact = document.getElementById("contact-section");
 
+//open interactive nav on click
+$(seeMore).click(function () {
+  if (interactiveNav.classList.contains("-translate-x-full")) {
+    interactiveNav.classList.remove("-translate-x-full");
+    interactiveNav.classList.add("translate-x-0");
+  } else {
+    interactiveNav.classList.add("-translate-x-full");
+    interactiveNav.classList.remove("translate-x-0");
+  }
+});
+
+//set innerHTML of interactive nav
+$(document).scroll(function () {
+  const currentLocation = $(interactiveNav).offset();
+  const workLocation = $(myWork).offset();
+  const experienceLocation = $(experience).offset();
+  const contactLocation = $(contact).offset();
+  if (currentLocation.top >= contactLocation.top) {
+    $(interactiveNav).html(sideNavData[3].innerHtml);
+  } else if (currentLocation.top >= experienceLocation.top) {
+    $(interactiveNav).html(sideNavData[2].innerHtml);
+  } else if (currentLocation.top >= workLocation.top) {
+    $(interactiveNav).html(sideNavData[1].innerHtml);
+  } else $(interactiveNav).html(sideNavData[0].innerHtml);
+});
+
+//load box function
 $(window).on("load", function () {
-  $(loadBox).delay(6000).fadeOut(1000);
+  $(loadBox).delay(5000).fadeOut(1000);
 
   $(quoteOne).addClass("active");
   $(quoteTwo).addClass("active");
@@ -15,16 +48,38 @@ $(window).on("load", function () {
   setTimeout(function () {
     $("body").removeClass("h-full overflow-hidden");
     $("html").removeClass("h-full overflow-hidden");
-    $(footer).addClass("opacity-50").removeClass("opacity-0");
-  }, 8500);
+  }, 7500);
 
   setTimeout(function () {
     $(feDev).addClass("opacity-100 flipY").removeClass("opacity-0");
     $(opera).addClass("opacity-80").removeClass("opacity-0");
     $(dogMom).addClass("opacity-60").removeClass("opacity-0");
-  }, 6500);
+  }, 6000);
 });
 
+//scroll smooth to navigation links
+function scrollToAnchor(targetId) {
+  var aTag = $("div[id='" + targetId + "']");
+  $("html,body").animate({ scrollTop: aTag.offset().top - 10 }, "slow");
+}
+
+$("#link-home").click(function () {
+  scrollToAnchor("home-section");
+});
+
+$("#link-work").click(function () {
+  scrollToAnchor("my-work-section");
+});
+
+$("#link-experience").click(function () {
+  scrollToAnchor("experience-section");
+});
+
+$("#link-contact").click(function () {
+  scrollToAnchor("contact-section");
+});
+
+//fade in elements on page scroll
 $(document).ready(function () {
   $(window).scroll(function () {
     $(".fade").each(function (i) {
---
+++ src/sideNavData.js
@@ -0,0 +1,64 @@
+export default [
+  {
+    id: 0,
+    relatedSection: "Home Section",
+    innerHtml: `<h3 class="font-sans text-sageGreen uppercase text-center mb-3 text-sm font-bold  md:text-m">
+      Things I do <br />(other than tech)
+    </h3>
+    <ul class=" text-xs md:text-base  text-center">
+      <li class="fun-thing mb-2">disc golf</li>
+      <li class="fun-thing mb-2">hiking</li>
+      <li class="fun-thing mb-2">write poetry</li>
+      <li class="fun-thing mb-2">gaming</li>
+      <li class="fun-thing mb-2">spin class!</li>
+      <li class="fun-thing mb-2">camping</li>
+      <li class="fun-thing mb-2">checking out art museums</li>
+      <li class="fun-thing mb-2">opera singing</li>
+      <li class="fun-thing mb-2">volleyball in the backyard</li>
+      <li class="fun-thing mb-2">puzzles</li>
+      <li class="fun-thing mb-2">rock climbing</li>
+      <li class="fun-thing mb-2">baking cookies</li>
+      <li class="fun-thing mb-2">learning new languages</li>
+      <li class="fun-thing mb-2">hanging with my pup, Freya</li>
+    </ul>`,
+  },
+  {
+    id: 1,
+    relatedSection: "My Work Section",
+
+    innerHtml: `<h3 class="font-sans text-sageGreen uppercase text-center text-sm font-bold mb-3 md:text-m">
+    Current Works in Progress
+  </h3>
+  <ul class="text-center text-xs md:text-base ">
+    <li class="wip py-2 border-b-[1px] border-sageGreen">Pokemon Team Builder</li>
+    <li class="wip py-2 border-b-[1px] border-sageGreen">JavaScript Card Game</li>
+    <li class="wip py-2 border-b-[1px] border-sageGreen">Tic Tac Toe Game</li>
+    <li class="wip py-2 border-b-[1px] border-sageGreen">React/Redux Course</li>
+    <li class="wip py-2 border-b-[1px] border-sageGreen">TypeScript Course</li>
+
+   
+  </ul>`,
+  },
+  {
+    id: 2,
+    relatedSection: "Experience Section",
+
+    innerHtml: `<h3 class="font-sans text-sageGreen uppercase text-center text-sm font-bold mb-3 md:text-m">
+    Favorites in My Tech Stack
+  </h3>
+  <ul class="text-center text-xs md:text-base ">
+    <li class="fav mb-2">React <span class="text-sageGreen block">(it's growing on me)</span></li>
+    <li class="fav mb-2">Figma <span class="text-sageGreen block">(easy to use, super detailed)</li>
+    <li class="fav mb-2">Tailwind <span class="text-sageGreen block">(excellent, just remember it's font-bold, not text-bold!)</li>
+    <li class="fav mb-2">jQuery<span class="text-sageGreen block">(intuitive and readable)</li>
+
+   
+  </ul>`,
+  },
+  {
+    id: 3,
+    relatedSection: "Contact Section",
+
+    innerHtml: `<h3 class="uppercase text-center font-bold text-sageGreen">Thanks for stopping by! <i class="fa-regular fa-face-smile-beam"></i></h3><p class="text-center text-whitish">I look forward to hearing from you!</p>`,
+  },
+];
